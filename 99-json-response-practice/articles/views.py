7280a4cf1b5b67a0dbe219ad3b4bab8d4df4eb25
Leejiwoo <easywoo980902@gmail.com>
2025-04-16 10:06:41 +0900

    submit

--- 99-json-response-practice/articles/views.py
+++ 99-json-response-practice/articles/views.py
@@ -8,7 +8,10 @@ from .models import Article
 
 # @api_view(['GET'])
 @api_view()
-def article_json(request):
-    articles = Article.objects.all()
+def article_json(request):  #인자
+    articles = Article.objects.all()    #게시글 조회
     serializer = ArticleSerializer(articles, many=True)
     return Response(serializer.data)
+    # form, 렌더 ,리다이렉트 없음
+    # 데이터를 가공 해서 응답
+    # 전체 데이터를 json으로 준거임
